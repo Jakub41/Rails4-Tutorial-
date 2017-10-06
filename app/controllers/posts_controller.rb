c0cab1882429bbc3d649361c510136e50df5628c
Jakub41 <lemiszewski@gmx.com>
2017-10-06 10:56:12 +0200

    Added actions to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,31 @@
 class PostsController < ApplicationController
   def index
+    @content_first = 'Simple text for testing'
+    @content_second = 'Another sample text'
+  end
+
+  def new
 
   end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
 end
