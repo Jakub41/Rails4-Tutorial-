c7c2877b56ad8038a717303321a4d01e4b2866bd
Jakub41 <lemiszewski@gmx.com>
2017-10-11 10:24:18 +0200

    Added Admin Panel and functionality

---
+++ app/admin/dashboard.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register_page "Dashboard" do
+  content :title => proc{ I18n.t("active_admin.dashboard") } do
+    columns do
+      column do
+        panel "Recent Posts" do
+          table_for Post.order("id desc").limit(5) do
+            column :name do |post|
+              link_to post.title, [:admin, post]
+            end
+            column :created_at
+          end
+          strong (link_to "Show All Posts" , :admin_posts)
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get 'categories/index'
 
   get 'categories/edit'
@@ -10,4 +13,5 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :posts
   resources :categories
+
 end
