5d65e4ff913e00e06cb627de486181f2da383239
Jakub41 <lemiszewski@gmx.com>
2017-10-11 11:12:54 +0200

    Updated and fixed admin functionality

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -7,9 +7,24 @@ ActiveAdmin.register_page "Dashboard" do
             column :name do |post|
               link_to post.title, [:admin, post]
             end
+            column :category
             column :created_at
           end
           strong (link_to "Show All Posts" , :admin_posts)
+        end
+      end
+    end
+
+    columns do
+      column do
+        panel "Categories" do
+          table_for Category.order("id desc").limit(5) do
+            column :name do |category|
+              link_to category.name, [:admin, category]
+            end
+            column :created_at
+          end
+          strong (link_to "Show All Categories" , :admin_categories)
         end
       end
     end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,4 +9,31 @@ class CategoriesController < ApplicationController
     @posts = @category.posts
   end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to category_path, notice: 'Category saved'
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      redirect_to categories_path, notice: 'Your category have been update'
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:category, :name)
+  end
 end
